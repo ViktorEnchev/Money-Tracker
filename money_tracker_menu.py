acc6507ce02e58e914a5ffffa327fc504dc865d2
Viktor Enchev <viktor-work@abv.bg>
2018-05-17 16:24:01 +0300

    Made slide changes to money tracker menu. Now it shows wrong input messages

--- money_tracker_menu.py
+++ money_tracker_menu.py
@@ -1,5 +1,6 @@
 from money_tracker import MoneyTracker
 import datetime
+import os
 
 
 class MTMenu:
@@ -17,11 +18,15 @@ class MTMenu:
             correct_date = True
         except ValueError:
             correct_date = False
+
         return correct_date
 
     def show_data_for_specific_date(self, day, month, year):
         if not self.check_date(year, month, day):
-            raise ValueError("Wrong date")
+            print("Wrong date!")
+            input('Continue')
+            raise ValueError
+
         else:
             if int(day) < 10:
                 day = '0{0}'.format(day)
@@ -34,38 +39,57 @@ class MTMenu:
         self.mt.list_user_expenses_ordered_by_categories(self.__data_dict)
 
     def checking_for_correctnes(self, amount, date):
+        os.system('clear')
+
         try:
             float(amount)
-        except ValueError as e:
-            raise e("Amount isn't a number!")
+        except ValueError:
+            print("Amount isn't a number!")
+            input('Continue')
+
         if float(amount) < 0:
-            raise ValueError("Amount is negative!")
+            print("Amount is negative!")
+            input('Continue')
+            raise ValueError
 
         day, month, year = date.split('-')
         if not self.check_date(year, month, day):
-            raise ValueError("Wrong Date!")
+            print("Wrong Date!")
+            input('Continue')
+            raise ValueError
+
         return True
 
     def add_inc(self):
-        amount = input("New income amount: ")
-        category = input("New income type: ")
-        print("Date should look like this: xx-xx-xxxx")
-        date = input("New income date: ")
-        try:
-            self.checking_for_correctnes(amount, date)
-            self.mt.add_income(category, amount, date, self.__data_dict)
-        except ValueError as e:
-            raise e
-            return False
+        while True:
+            os.system('clear')
+
+            amount = input("New income amount: ")
+            category = input("New income type: ")
+            print("Date should look like this: xx-xx-xxxx")
+            date = input("New income date: ")
+
+            try:
+                self.checking_for_correctnes(amount, date)
+                self.mt.add_income(category, amount, date, self.__data_dict)
+            except ValueError:
+                continue
+            else:
+                break
 
     def add_exp(self):
-        amount = input("New expense amount: ")
-        category = input("New expense type: ")
-        print("Date should look like this: xx-xx-xxxx")
-        date = input("New expense date: ")
-        try:
-            self.checking_for_correctnes(amount, date)
-            self.mt.add_expense(category, amount, date, self.__data_dict)
-        except ValueError as e:
-            raise e
-            return False
+        while True:
+            os.system('clear')
+
+            amount = input("New expense amount: ")
+            category = input("New expense type: ")
+            print("Date should look like this: xx-xx-xxxx")
+            date = input("New expense date: ")
+
+            try:
+                self.checking_for_correctnes(amount, date)
+                self.mt.add_expense(category, amount, date, self.__data_dict)
+            except ValueError:
+                continue
+            else:
+                break
